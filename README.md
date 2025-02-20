29bb3c4dd52de40f1b0a5d94e23fbdb9845b6483
Nathan Long <him@nathanmlong.com>
2025-02-20 14:01:28 -0500

    delete unused code and update readme

--- README.md
+++ README.md
@@ -13,3 +13,14 @@ Do the above, then run `iex -S mix` and play with:
 
 - `LcDemo.doc_example/1`
 - `LcDemo.monster_query/1`.
+
+## What Is this Magic?
+
+I asked ChatGPT, and it said that what LangChain does is "tool augmentation or agent-based interaction."
+
+> ## How It Works in Practice:
+- **Prompting the Model**: The model is given a prompt that not only includes the current conversation but also a description of available functions (e.g., “updateDatabase” or “queryDatabase”).
+- **Deciding to Act**: Based on the conversation context, the model determines that it needs to perform an action rather than just generate text.
+- **Generating a Function Call**: The model outputs a command in a structured format that specifies which function to call and with what parameters.
+- **Executing and Returning Results**: LangChain or your custom middleware intercepts this command, executes the corresponding function, and then provides the output back into the conversation.
+- **Continuing the Dialogue**: The model uses this returned data to inform further responses.
--- lib/lc_demo/monsters.ex
+++ lib/lc_demo/monsters.ex
@@ -9,35 +9,6 @@ defmodule LcDemo.Monsters do
   alias LcDemo.Monsters.Monster
 
   @doc """
-  Returns the list of monsters.
-
-  ## Examples
-
-      iex> list_monsters()
-      [%Monster{}, ...]
-
-  """
-  def list_monsters do
-    Repo.all(Monster)
-  end
-
-  @doc """
-  Gets a single monster.
-
-  Raises `Ecto.NoResultsError` if the Monster does not exist.
-
-  ## Examples
-
-      iex> get_monster!(123)
-      %Monster{}
-
-      iex> get_monster!(456)
-      ** (Ecto.NoResultsError)
-
-  """
-  def get_monster!(id), do: Repo.get!(Monster, id)
-
-  @doc """
   Finds a monster by name.
 
   ## Examples
@@ -97,52 +68,5 @@ defmodule LcDemo.Monsters do
     %Monster{}
     |> Monster.changeset(attrs)
     |> Repo.insert()
-  end
-
-  @doc """
-  Updates a monster.
-
-  ## Examples
-
-      iex> update_monster(monster, %{field: new_value})
-      {:ok, %Monster{}}
-
-      iex> update_monster(monster, %{field: bad_value})
-      {:error, %Ecto.Changeset{}}
-
-  """
-  def update_monster(%Monster{} = monster, attrs) do
-    monster
-    |> Monster.changeset(attrs)
-    |> Repo.update()
-  end
-
-  @doc """
-  Deletes a monster.
-
-  ## Examples
-
-      iex> delete_monster(monster)
-      {:ok, %Monster{}}
-
-      iex> delete_monster(monster)
-      {:error, %Ecto.Changeset{}}
-
-  """
-  def delete_monster(%Monster{} = monster) do
-    Repo.delete(monster)
-  end
-
-  @doc """
-  Returns an `%Ecto.Changeset{}` for tracking monster changes.
-
-  ## Examples
-
-      iex> change_monster(monster)
-      %Ecto.Changeset{data: %Monster{}}
-
-  """
-  def change_monster(%Monster{} = monster, attrs \\ %{}) do
-    Monster.changeset(monster, attrs)
   end
 end
